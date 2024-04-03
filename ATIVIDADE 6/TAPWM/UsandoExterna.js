e2f3a720130e63fff181559aedd36c1d9f6a27eb
Filipe Fogaça <109300270+F1lipeFGC@users.noreply.github.com>
2024-04-03 15:16:12 -0300

    Add files via upload

--- ATIVIDADE 6/TAPWM/UsandoExterna.js
+++ ATIVIDADE 6/TAPWM/UsandoExterna.js
@@ -1,1 +1,10 @@
-document.write("Esta é uma forma de usar JavaScript Externo")
+document.write('Está é uma forma de usar JavaScript externo')
+
+var nome = prompt("Digite seu nome: ")
+var primeira_nota = prompt("digite a primeira nota: ")
+var segunda_nota = prompt("digite a segunda nota: ")
+var terceira_nota = prompt("digite a terceira nota: ")
+
+const resposta = ((parseFloat(primeira_nota) + parseFloat(primeira_nota) + parseFloat(terceira_nota)) / 3);
+
+alert(nome + " sua média foi: " + resposta);
