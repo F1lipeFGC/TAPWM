1e2a10c1016444131838eaa5c8e41f03691711f7
FilipeFogaca <filipefogaca4@gmail.com>
2024-08-27 21:37:37 -0300

    atividades prontas :)

--- Atividade 14/JS14.js
+++ Atividade 14/JS14.js
@@ -29,122 +29,121 @@ function criarERetornarRetangulo() {
 }
 
 
-function Conta(nome, banco, numero, saldo) {
-    this._nome = nome;
-    this._banco = banco;
-    this._numero = numero;
-    this._saldo = saldo;
-}
+class Conta {
+    constructor(nomeCorrentista, banco, numeroConta, saldo) {
+        this.nomeCorrentista = nomeCorrentista;
+        this.banco = banco;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
 
-Conta.prototype = {
-    get nome() {
-        return this._nome;
-    },
-    set nome(value) {
-        this._nome = value;
-    },
+    get nomeCorrentista() {
+        return this._nomeCorrentista;
+    }
+
+    set nomeCorrentista(valor) {
+        this._nomeCorrentista = valor;
+    }
+
     get banco() {
         return this._banco;
-    },
-    set banco(value) {
-        this._banco = value;
-    },
-    get numero() {
-        return this._numero;
-    },
-    set numero(value) {
-        this._numero = value;
-    },
+    }
+
+    set banco(valor) {
+        this._banco = valor;
+    }
+
+    get numeroConta() {
+        return this._numeroConta;
+    }
+
+    set numeroConta(valor) {
+        this._numeroConta = valor;
+    }
+
     get saldo() {
         return this._saldo;
-    },
-    set saldo(value) {
-        this._saldo = value;
-    },
-    exibirDados() {
-        return `Nome: ${this._nome}, Banco: ${this._banco}, Número: ${this._numero}, Saldo: ${this._saldo}`;
     }
-};
 
-// Função construtora para Conta Corrente com Saldo Especial
-function ContaCorrente(nome, banco, numero, saldo, saldoEspecial) {
-    Conta.call(this, nome, banco, numero, saldo);
-    this._saldoEspecial = saldoEspecial;
+    set saldo(valor) {
+        this._saldo = valor;
+    }
+
+    exibirDados() {
+        return `Nome: ${this.nomeCorrentista}\nBanco: ${this.banco}\nNúmero da Conta: ${this.numeroConta}\nSaldo: R$ ${this.saldo.toFixed(2)}`;
+    }
 }
 
-// Herança e métodos get e set para ContaCorrente
-ContaCorrente.prototype = Object.create(Conta.prototype);
-ContaCorrente.prototype.constructor = ContaCorrente;
+class Corrente extends Conta {
+    constructor(nomeCorrentista, banco, numeroConta, saldo, saldoEspecial) {
+        super(nomeCorrentista, banco, numeroConta, saldo);
+        this.saldoEspecial = saldoEspecial;
+    }
 
-ContaCorrente.prototype = {
     get saldoEspecial() {
         return this._saldoEspecial;
-    },
-    set saldoEspecial(value) {
-        this._saldoEspecial = value;
-    },
-    exibirDados() {
-        return `${super.exibirDados()}, Saldo Especial: ${this._saldoEspecial}`;
     }
-};
 
-// Função construtora para Conta Poupança com Juros e Data de Vencimento
-function ContaPoupanca(nome, banco, numero, saldo, juros, dataVencimento) {
-    Conta.call(this, nome, banco, numero, saldo);
-    this._juros = juros;
-    this._dataVencimento = dataVencimento;
+    set saldoEspecial(valor) {
+        this._saldoEspecial = valor;
+    }
+
+    exibirDados() {
+        return `${super.exibirDados()}\nSaldo Especial: R$ ${this.saldoEspecial.toFixed(2)}`;
+    }
 }
 
-// Herança e métodos get e set para ContaPoupanca
-ContaPoupanca.prototype = Object.create(Conta.prototype);
-ContaPoupanca.prototype.constructor = ContaPoupanca;
+class Poupanca extends Conta {
+    constructor(nomeCorrentista, banco, numeroConta, saldo, taxaJuros, dataVencimento) {
+        super(nomeCorrentista, banco, numeroConta, saldo);
+        this.taxaJuros = taxaJuros;
+        this.dataVencimento = dataVencimento;
+    }
 
-ContaPoupanca.prototype = {
-    get juros() {
-        return this._juros;
-    },
-    set juros(value) {
-        this._juros = value;
-    },
+    get taxaJuros() {
+        return this._taxaJuros;
+    }
+
+    set taxaJuros(valor) {
+        this._taxaJuros = valor;
+    }
+
     get dataVencimento() {
         return this._dataVencimento;
-    },
-    set dataVencimento(value) {
-        this._dataVencimento = value;
-    },
-    exibirDados() {
-        return `${super.exibirDados()}, Juros: ${this._juros}, Data de Vencimento: ${this._dataVencimento}`;
     }
-};
 
-// Aqui esta  segunda ATV
-function contaCorrente() {
+    set dataVencimento(valor) {
+        this._dataVencimento = valor;
+    }
 
-    const nomeCorrente = prompt("Digite o nome do correntista da Conta Corrente:");
+    exibirDados() {
+        return `${super.exibirDados()}\nTaxa de Juros: ${this.taxaJuros.toFixed(2)}%\nData de Vencimento: ${this.dataVencimento}`;
+    }
+}
+
+function Contas() {
+    const nomeCorrentistaCorrente = prompt("Digite o nome do correntista da Conta Corrente:");
     const bancoCorrente = prompt("Digite o banco da Conta Corrente:");
-    const numeroContaCorrente = prompt("Digite o número da Conta Corrente:");
+    const numeroContaCorrente = prompt("Digite o número da conta da Conta Corrente:");
     const saldoCorrente = parseFloat(prompt("Digite o saldo da Conta Corrente:"));
     const saldoEspecial = parseFloat(prompt("Digite o saldo especial da Conta Corrente:"));
 
+    const contaCorrente = new Corrente(nomeCorrentistaCorrente, bancoCorrente, numeroContaCorrente, saldoCorrente, saldoEspecial);
 
-    const contaCorrente = new ContaCorrente(nomeCorrente, bancoCorrente, numeroContaCorrente, saldoCorrente, saldoEspecial);
-    alert(contaCorrente.exibirDados());
-
-    function contaPoupanca(){
-
-    const nomePoupanca = prompt("Digite o nome do correntista da Conta Poupança:");
+    const nomeCorrentistaPoupanca = prompt("Digite o nome do correntista da Conta Poupança:");
     const bancoPoupanca = prompt("Digite o banco da Conta Poupança:");
-    const numeroContaPoupanca = prompt("Digite o número da Conta Poupança:");
+    const numeroContaPoupanca = prompt("Digite o número da conta da Conta Poupança:");
     const saldoPoupanca = parseFloat(prompt("Digite o saldo da Conta Poupança:"));
-    const juros = parseFloat(prompt("Digite a taxa de juros da Conta Poupança:"));
-    const dataVencimento = prompt("Digite a data de vencimento da Conta Poupança (formato YYYY-MM-DD):");
+    const taxaJuros = parseFloat(prompt("Digite a taxa de juros da Conta Poupança (em %):"));
+    const dataVencimento = prompt("Digite a data de vencimento da Conta Poupança (YYYY-MM-DD):");
 
+    const contaPoupanca = new Poupanca(nomeCorrentistaPoupanca, bancoPoupanca, numeroContaPoupanca, saldoPoupanca, taxaJuros, dataVencimento);
 
-    const contaPoupanca = new ContaPoupanca(nomePoupanca, bancoPoupanca, numeroContaPoupanca, saldoPoupanca, juros, dataVencimento);
-    alert(contaPoupanca.exibirDados());
-
-    }
+    alert('Dados da Conta Corrente:\n' + contaCorrente.exibirDados());
+    alert('Dados da Conta Poupança:\n' + contaPoupanca.exibirDados());
 }
+
+
 
 
 
